259a6fb838c032ad0531d205b1505c277bbf263e
prodval <157516871+timecarriesme@users.noreply.github.com>
2024-01-23 23:34:47 +0600

    Add files via upload

---
+++ nominal number.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nominalnumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите третье число: ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            if (num1 >= num2 && num1 <= num3 || num1 >= num3 && num1 <= num2)
+                Console.WriteLine(num1);
+            else if (num2 >= num1 && num2 <= num3 || num2 >= num3 && num2 <= num1)
+                Console.WriteLine(num2);
+            else 
+                Console.WriteLine(num3);
+
+            Console.Write("Нажмите Enter для завершения работы программы.");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) {}
+        }
+    }
+}
